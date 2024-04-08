64994969d75ea127e2ade0ffa0b26c72fff6d156
Marek <marekgacek45@gmail.com>
2024-04-08 10:11:48 +0200

    install tailwind and add own fonts

---
+++ resources/views/components/layouts/main.blade.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <!--Title-->
+    <title>@yield('title', 'Tatrzański Raj')</title>
+    <meta name="description" content='@yield('description')'>
+    <!--Cannonical-->
+    <link rel="canonical" href="{{ url()->current() }}" />
+    {{-- <!--Favicons-->
+    @include('partials.favicon')
+    <!--Facebook Meta-->
+    @include('partials.facebook-meta')
+    <!--Fonts-->
+    @include('partials.fonts') --}}
+    <!-- Scripts -->
+    @vite(['resources/scss/app.scss', 'resources/js/app.js'])
+
+</head>
+
+{{-- <body class="overflow-x-hidden"> --}}
+<body >
+
+    {{-- <!--NAVIGATION-->
+    @include('shared.nav.top-bar')
+    @include('shared.nav.nav-bar')
+    @include('shared.nav.mobile-menu')
+    <!--PRELOADER-->
+    @include('shared.preloader')
+
+    <div> --}}
+
+        {{ $slot }}
+    {{-- </div>
+
+    <!--FOOTER-->
+
+    @include('shared.footer')
+    @include('shared.mobile-buttons') --}}
+
+    <!--SCRIPTS-->
+    {{-- <script src="https://wis.upperbooking.com/owcedwie/be-panel?locale=pl" async></script> --}}
+
+
+
+</body>
+
+</html>
---
+++ resources/views/pages/home/index.blade.php
@@ -0,0 +1,14 @@
+<x-layouts.main>
+
+    {{-- @section('title', __('home.title'))
+    @section('description', __('home.description')) --}}
+    @section('title', 'Tatrzański Raj: Luksusowy Hotel w Sercu Tatr - Idealne Miejsce na Odpoczynek')
+    @section('description', 'Odkryj luksus Tatrzańskiego Raju – Twojego wymarzonego hotelu w górach. Doskonała
+        lokalizacja, komfortowe pokoje i niezapomniane widoki. Zarezerwuj teraz!')
+
+        <p class="text-red-600 text-xl text-center mt-32 font-bold">test</p>
+    <span>text-center</span>
+
+
+
+    </x-layouts.main>
