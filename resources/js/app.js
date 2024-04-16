270ff8c86418f4ea1fb6fa0560b2479119e81d74
Marek <marekgacek45@gmail.com>
2024-04-16 14:25:06 +0200

    finish features section

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,22 +1,14 @@
 import './bootstrap';
 import './partials/hamburger'
 import './partials/navbar'
+import './partials/featuresBg'
 import './swiper/description_images'
 
 
 
 
 
-var container = document.getElementById('container');
 
-document.querySelectorAll('.hover\\:bg').forEach((div) => {
-    div.addEventListener('mouseover', function() {
-        container.style.backgroundImage = 'url(' + this.dataset.bg + ')';
-    });
-    div.addEventListener('mouseout', function() {
-        container.style.backgroundImage = 'url(/assets/images/desktop/willaPodScietaLipa-25.jpg)';
-    });
-});
 
 
 
---
+++ resources/views/pages/home/sections/features.blade.php
@@ -0,0 +1,25 @@
+<section class="py-10 2xl:py-20">
+<div id="container" class="bg-[url('/public/assets/images/features/willaPodScietaLipa-4.jpg')] h-[90vh] bg-gray-400 bg-blend-multiply grid lg:grid-cols-4 duration-300 bg-no-repeat bg-cover bg-center">
+
+    <div class="h-full flex justify-center items-center hover:bg" data-bg="/assets/images/features/willaPodScietaLipa-2.jpg">
+        <a href="" class="font-JazminThin text-4xl 2xl:text-5xl text-center text-fontLight">Sala zabaw dla Dzieci</a>
+    </div>
+
+    <div class="h-full flex justify-center items-center border-t lg:border-l border-b lg:border-r border-white hover:bg" data-bg="/assets/images/features/willaPodScietaLipa-4.jpg">
+        <a href="" class=" font-JazminThin text-4xl 2xl:text-5xl text-center text-fontLight">Widok na Tatry</a>
+    </div>
+
+    <div class="h-full flex justify-center border-b lg:border-r items-center hover:bg" data-bg="/assets/images/features/willaPodScietaLipa-3.jpg">
+        <a href="" class="font-JazminThin text-4xl 2xl:text-5xl text-center text-fontLight">Duży taras widokowy</a>
+    </div>
+    <div class="h-full flex justify-center items-center hover:bg" data-bg="/assets/images/features/willaPodScietaLipa-1.jpg">
+        <a href="" class="font-JazminThin text-4xl 2xl:text-5xl text-center text-fontLight">Salon z kominkiem</a>
+    </div>
+
+</div>
+
+</section>
+
+
+
+
