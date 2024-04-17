1ae9db19c556d9ad1c85daf5782ad0bf87964b3d
Marek <marekgacek45@gmail.com>
2024-04-17 14:35:53 +0200

    add smooth scroll

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,8 +6,10 @@ import './partials/mobileButtons'
 import './partials/footerYear'
 import './partials/preloader'
 import './partials/gLightbox'
+import './partials/smoothScroll'
 import './swiper/description_images'
 import './swiper/home_gallery'
+
 
 
 
